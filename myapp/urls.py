89300d4b326de13157428a662687bee9a3d56bf4
Ashboorrnn <113339241+Ashboorrnn@users.noreply.github.com>
2024-11-06 11:43:05 +0800

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),    #new
     path('detection/video_feed/', views.video_feed, name='video_feed'),
     path('detection/cleanup/', views.cleanup_view, name='cleanup'),
     path('detection/get_prediction/', views.get_prediction, name='get_prediction'),
